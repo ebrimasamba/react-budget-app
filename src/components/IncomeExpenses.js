4ff9db8bed05962cc5ead5205c0681f356c1b743
Ebrima Samba <ebrima.samba.75@gmail.com>
2020-05-07 01:07:25 +0000

    removing icons when in mobile

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -50,12 +50,12 @@ export const IncomeExpenses = () => {
     <div className="flex justify-center mt-2 rounded-md text-sm ">
       <div className="bg-blue-gradient text-gray-100 text-center w-1/2 rounded-md border-l-4 border-blue-600 p-3 relative shadow">
         <div className="relative z-10">
-          <h5 className="uppercase font-bold ">Income</h5>
+          <h5 className="uppercase font-bold">Income</h5>
           <span className="text font-bold text-xl">
             +D{Math.abs(income).toFixed(2)}
           </span>
         </div>
-        <FaMoneyBill className="absolute text-blue-400 text-4xl top-5 left-3"></FaMoneyBill>
+        <FaMoneyBill className="absolute text-blue-400 text-4xl top-5 left-3 hidden md:block"></FaMoneyBill>
       </div>
       <div className="bg-red-gradient text-gray-100 text-center w-1/2 rounded-md border-l-4 border-red-600 p-3 relative ml-2 shadow">
         <div className="relative z-10">
@@ -64,7 +64,7 @@ export const IncomeExpenses = () => {
             -D{Math.abs(expense).toFixed(2)}
           </span>
         </div>
-        <FaShoppingBag className="absolute text-red-400 text-4xl top-5 left-3"></FaShoppingBag>
+        <FaShoppingBag className="absolute text-red-400 text-4xl top-5 left-3 hidden md:block"></FaShoppingBag>
       </div>
     </div>
   );
